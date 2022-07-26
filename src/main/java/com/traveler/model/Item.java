393642fb3b5548d47681702315602f1b61ff2793
JoeyHJava <3studyemail@gmail.com>
2022-07-26 14:24:15 -0700

    AB176 add

--- src/main/java/com/traveler/model/Item.java
+++ src/main/java/com/traveler/model/Item.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class Item{
     String name;
@@ -36,4 +37,81 @@ public class Item{
         }
         System.out.println(noun + " not found");
     }
+
+    //Optional used to avoid returning null
+    public Optional<Item> cmdGetItem(String noun) {
+        Optional<Item> requestedItem = Optional.empty();
+        if(noun != null && !noun.isEmpty()){
+            for (Item item: inventory
+            ) {
+                System.out.println(item.name);
+                if(noun.equals(item.name)){
+                    var takeItem = inventory.remove(inventory.indexOf(item));
+                    requestedItem = Optional.of(item);
+                } else {
+                    /*
+                     * will return Optional.empty if noun is not in inventory
+                     * */
+                    requestedItem = Optional.empty();
+                }
+            }
+        }
+        return requestedItem;
+    }
+
+    public Optional<String> cmdDropItem(Item droppedItem){
+        Optional<String> requestedDropItem = Optional.empty();
+        if(droppedItem != null){
+//           TODO: check if item in player inventory
+//            addItem(Item addItem)
+//           TODO: replace removing from Item inventory with Player inventory
+            //code will be similar to cmdGetItem here
+            //get from player, goes to room inventory
+            //consider
+            //Room.item is a list
+            //current room
+        }
+
+        return requestedDropItem;
+    }
+
+    /*
+    * this is a helper method for now, called by cmdDropItem
+    * when the item is dropped by the player,
+    * it will be added to the current room's inventory
+    * */
+    public String addItem(Item addItem){
+        inventory.add(addItem);
+        return addItem.name + " was dropped";
+    }
+
+    public String cmdUseItem(String noun){
+        String message = "";
+//        TODO: check if item is in Players inventory
+//        If item in players inventory and single use, remove item from players inventory
+//        and return a string message to user
+//        Else return message informing player item is not in players inventory
+        /*
+        * Will/there will be single use and multi-use items
+        * If item is one time use, it will be removed from players inventory
+        * items will a field designating item as single or multi-use item
+        * */
+        return message;
+    }
+//    TODO: remove after testing
+//    public static void main(String[] args) {
+//        Item item = new Item();
+//        try {
+//            Item.fromJsonToArray();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        var returnedItem = item.cmdGetItem("ksey");
+//        //this is not the anticipated flow of the game.
+//        //Only for testing
+//        if(!returnedItem.isEmpty()){
+//            var test = item.addItem(returnedItem.get());
+//            System.out.println("TEST " + test);
+//        }
+//    }
 }
