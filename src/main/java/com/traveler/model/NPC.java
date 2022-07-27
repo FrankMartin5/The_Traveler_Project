0b8c594615d4aa47ae7449afd2760a934dbad489
Mabel Egba <megba2014@gmail.com>
2022-07-27 11:23:09 -0500

    npc Json created and cmdTalk gets npc talk, AB#168, AB#172

---
+++ src/main/java/com/traveler/model/NPC.java
@@ -0,0 +1,58 @@
+package com.traveler.model;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+class NPC {
+    String name;
+    String talk;
+
+    // Creates npc which carries NPC objects
+    public static List<NPC> npcList;
+
+    // method that reads from json file and loads npc with NPC objects
+    public static List<NPC> NPCArray() throws IOException {
+        Gson gson = new Gson();
+        Type itemListType = new TypeToken<List<NPC>>() {}.getType();
+        npcList = new Gson().fromJson(new FileReader("src/main/resources/npc.json"), itemListType);
+        return npcList;
+    }
+
+    // when command is "talk <npc>" returns talk
+    public static void cmdTalk(String noun) {
+        for(NPC i: npcList){
+            if (i.name.equals(noun)) {
+                System.out.println(i.talk);
+                return;
+            }
+        }
+        System.out.println(noun + " not found");
+    }
+
+    @Override
+    public String toString() {
+        return "NPC{" +
+                "name='" + name + '\'' +
+                ", talk='" + talk + '\'' +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+
+        NPC npc1 = new NPC();
+        try {
+            NPCArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(npcList);
+        NPC.cmdTalk("elon musk");
+
+    }
+}
