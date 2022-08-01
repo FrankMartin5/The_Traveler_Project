95c7d76b0c15f16a52e72fa4242762199ee88c08
Mabel Egba <megba2014@gmail.com>
2022-08-01 15:14:20 -0500

    Map of the game, AB#182

---
+++ src/main/java/com.traveler.view/Map.java
@@ -0,0 +1,25 @@
+package com.traveler.view;
+
+class Map {
+      public static void cmdMap(){
+          System.out.println("*----------------------------------------------------------------------------*");
+          System.out.println("*                          MAP OF RACUMEN'S LAIR                             *");
+          System.out.println("*                                                                            *");
+          System.out.println("*                           *----------------*         *---------------*     *");
+          System.out.println("*                           * Dining Chamber *  -----  * Ancient Study *     *");
+          System.out.println("*                           *----------------*         *---------------*     *");
+          System.out.println("*                                    *                                       *");
+          System.out.println("*                                    *                                       *");
+          System.out.println("*  *-------------*          *----------------*        *----------------*     *");
+          System.out.println("*  *  Crypt      * -----    *   Great Hall   *  ----- * Torture Chamber*     *");
+          System.out.println("*  *-------------*          *--------------- *        *----------------*     *");
+          System.out.println("*                                    *                                       *");
+          System.out.println("*                                    *                                       *");
+          System.out.println("*                           *----------------*                               *");
+          System.out.println("*                           *Warlocks chamber*                               *");
+          System.out.println("*                           *----------------*                               *");
+          System.out.println("*                                                                            *");
+          System.out.println("*----------------------------------------------------------------------------*");
+
+      }
+}
