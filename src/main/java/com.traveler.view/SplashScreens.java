f4a5c7e3aff93e8f48761510326904685f44e66d
Hoon <hoonhyungcho@gmail.com>
2022-08-01 13:23:20 -0700

    added win condition to logic, AB#178, players can only talk to NPC in room, AB#173, players can open a locked door, AB#171

--- src/main/java/com.traveler.view/SplashScreens.java
+++ src/main/java/com.traveler.view/SplashScreens.java
@@ -33,8 +33,34 @@ public class SplashScreens {
         }
     }
 
+    public static void win() {
+        int width = 300;
+        int height = 20;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("SansSerif", Font.BOLD, 14));
+
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString("Y O U  W I N", 10, 20);
+
+        for (int y = 0; y < height; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "!");
+            }
+
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+
+            System.out.println(sb);
+        }
+    }
+
     //    TODO: remove before commit.  For test purpose only
     public static void main(String[] args) {
-        SplashScreens.art();
+        SplashScreens.win();
     }
 }
